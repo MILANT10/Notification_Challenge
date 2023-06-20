b3367e375195e3435f18d46baadf9cc4d219a64a
Milan TURCANO <turcano.milan@sfr.fr>
2023-06-20 12:00:57 +0200

    Nouvelle version du code js avec counter et changement de backgound color avec tableau

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-let notif = document.querySelectorAll(".notifications");
+let nodeList = document.querySelectorAll(".notifications");
+let notif =  Array.from(nodeList);
+let firstThreeElements = notif.slice(0,3);
 let allRead = document.querySelector(".markRead");
 let countNotif = document.querySelector(".countNotif");
+let counter = 3;
 
-
-
+countNotif.textContent = counter;
 
 notif.forEach(el => {
 
-
+    firstThreeElements.forEach(el => {
+        el.style.backgroundColor = "hsl(211, 68%, 94%)";
+      });
     
 
     let classNotif = window.getComputedStyle(el);
@@ -17,8 +21,12 @@ notif.forEach(el => {
     
     el.addEventListener("click", () =>{
         if(backgroundNotif){
-            el.classList =("");
-            notif[7].remove();
+            el.style.backgroundColor = "white";
+            if(counter > 0){
+                counter--;
+                countNotif.textContent = counter;
+            }
+            
         } else{
             
         }
@@ -26,8 +34,10 @@ notif.forEach(el => {
 
     allRead.addEventListener("click", () =>{
     el.style.backgroundColor = "white";
+    counter = 0;
+    countNotif.textContent = counter;
     });
 
-    countNotif.textContent = notif.length.toString();
+    
     
 });
