564714ba5c64598aa8648fab4039d6ebfbcf4b80
Milan TURCANO <turcano.milan@sfr.fr>
2023-06-20 19:40:44 +0200

    Event JS redpoint click.

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
 let nodeList = document.querySelectorAll(".notifications");
-let notif =  Array.from(nodeList);
-let firstThreeElements = notif.slice(0,3);
+let notif = Array.from(nodeList);
+let firstThreeElements = notif.slice(0, 3);
 let allRead = document.querySelector(".markRead");
 let countNotif = document.querySelector(".countNotif");
-let redPoint = document.querySelectorAll(".eventNotif::after");
 let counter = 3;
 countNotif.textContent = counter;
 
 notif.forEach(el => {
+  firstThreeElements.forEach(el => {
+    el.style.backgroundColor = "hsl(211, 68%, 94%)";
+  });
 
-    firstThreeElements.forEach(el => {
-        el.style.backgroundColor = "hsl(211, 68%, 94%)";
-      });
-    
-
+  el.addEventListener("click", () => {
     let classNotif = window.getComputedStyle(el);
     let backgroundNotif = classNotif.getPropertyValue("background-color");
 
-    
-    el.addEventListener("click", () =>{
-        if(backgroundNotif){
-            el.style.backgroundColor = "white";
-            redPoint.style.display = "none";
-            if(counter > 0){
-                counter--;
-                countNotif.textContent = counter;
-            }
-            
-        } else{
-            
-        }
-    });
+    if (backgroundNotif) {
+      el.style.backgroundColor = "white";
+      el.classList.add("clicked");
+      let redPoints = el.querySelectorAll(".redPoint");
+      redPoints.forEach(redPoint => {
+        redPoint.style.display = "none";
+      });
+      if (counter > 0) {
+        counter--;
+        countNotif.textContent = counter;
+      }
+    }
+  });
 
-
-    allRead.addEventListener("click", () =>{
+  allRead.addEventListener("click", () => {
     el.style.backgroundColor = "white";
+    el.classList.add("clicked");
     counter = 0;
     countNotif.textContent = counter;
+    let redPoints = el.querySelectorAll(".redPoint");
+    redPoints.forEach(redPoint => {
+      redPoint.style.display = "none";
     });
-
-    
-    
+  });
 });
